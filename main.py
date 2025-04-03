5f318eb42e493c5ec6d921cefbe71efe740c7324
Josiah Weber <jtw497@gmail.com>
2025-04-03 13:23:11 -0700

    Add missing sprite group functionality. Add working asteroid field.

--- main.py
+++ main.py
@@ -1,30 +1,46 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
-    
-    # Game loop
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+
     while True:
-        dt = clock.tick(60) / 1000.0  # add dt calculation
-        
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+
+        screen.fill("black")
         
-        # Update and draw INSIDE the game loop
-        player.update(dt)  # pass dt to update
+        for obj in drawable:
+            obj.draw(screen)
         
-        screen.fill((0,0,0))
-        player.draw(screen)
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
